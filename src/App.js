6bbf89dd14ad4f65bb077e797c06028abf662add
kaschief <kaschiefj@gmail.com>
2018-12-13 12:50:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+import Main from './Main';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Sidebar />
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+  return <stuff className="Sidebar">Sidebar</stuff>;
+};
+
+export default Sidebar;
